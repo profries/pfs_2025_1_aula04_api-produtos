77912db19f39ec19f2a0311d066688dd8bb43a9f
Luis Henrique Ries <prof.ries@gmail.com>
2025-04-06 17:25:00 -0300

    Adicionados atualizar e deletar

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import { request } from 'http';
 import { Produto } from './types/produto';
 
 const app = express();
@@ -14,10 +13,12 @@ app.get('/hello', (req: Request, res: Response) => {
     res.json({ message: "Hello World" });
 })
 
+//listar produtos
 app.get('/api/produtos', (req: Request, res: Response) => {
     res.json(listaProdutos);
 })
 
+//inserir produto
 app.post('/api/produtos', async (req: Request, res: Response): Promise<any> => {
     const produto = req.body;
     if(!produto.nome || !produto.categoria || !produto.preco) {
@@ -35,6 +36,7 @@ app.post('/api/produtos', async (req: Request, res: Response): Promise<any> => {
     res.status(201).json(novoProduto);
 })
 
+//buscar produto por id
 app.get('/api/produtos/:id', async (req: Request, res: Response): Promise<any> => {
     const id = parseInt(req.params.id);
 
@@ -51,6 +53,43 @@ app.get('/api/produtos/:id', async (req: Request, res: Response): Promise<any> =
     res.json(produto);
 })
 
+//atualizar produto
+app.put('/api/produtos/:id', async (req: Request, res: Response): Promise<any> => {
+    const id = parseInt(req.params.id);
+    const { nome, categoria, preco } = req.body;
+    const produtoIndex = listaProdutos.findIndex(p => p.id === id);
+  
+    if (produtoIndex === -1) {
+      return res.status(404).json({ error: 'Produto Não encontrado' });
+    }
+  
+    if (!nome || !categoria || preco === undefined) {
+      return res.status(400).json({ error: 'Falta dados obrigatorios' });
+    }
+  
+    listaProdutos[produtoIndex] = {
+      ...listaProdutos[produtoIndex], //mantém os outros dados de produto (id)
+      nome,
+      categoria,
+      preco
+    };
+  
+    res.json(listaProdutos[produtoIndex]);
+  });
+  
+//deletar produto
+app.delete('/api/produtos/:id', async(req: Request, res: Response): Promise<any> => {
+    const id = parseInt(req.params.id);
+    const produtoIndex = listaProdutos.findIndex(p => p.id === id);
+  
+    if (produtoIndex === -1) {
+      return res.status(404).json({ error: 'produto not found' });
+    }
+  
+    let produtoDeletado = listaProdutos.splice(produtoIndex, 1);
+    res.json(produtoDeletado);
+});
+  
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
